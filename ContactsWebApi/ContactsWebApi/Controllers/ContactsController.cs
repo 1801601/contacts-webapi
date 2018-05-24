50dd3a81d4b8f07800c3c73242c6369ed991bab7
1801061 <1801061@co.saimia.fi>
2018-05-24 12:22:29 +0300

    Toimii jostain syystä

--- ContactsWebApi/ContactsWebApi/Controllers/ContactsController.cs
+++ ContactsWebApi/ContactsWebApi/Controllers/ContactsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContactsWebApi.Models;
+using ContactsWebApi.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,20 +13,28 @@ namespace ContactsWebApi.Controllers
     [Route("api/contacts")]
     public class ContactsController : Controller
     {
-        public ContactsController() { }
+        private readonly IContactService _contactService;
+
+        public ContactsController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
 
         // GET api/contacts
         [HttpGet]
         public IActionResult Get()
         {
-            return new JsonResult(new {firstName = "hip", lastName = "Object"});
+            List<Contact> contacts = _contactService.GetContacts();
+            return new JsonResult(contacts);
         }
 
         // GET api/contacts/{id}
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            return new OkResult();
+            Contact contact = _contactService.GetContactById(id);
+            return new JsonResult(contact);
         }
     }
 }
